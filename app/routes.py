656b75d3e4debaf09fe20cba278b0ec8acae7227
Mike Ly <mikely@Mikes-MacBook-Pro.local>
2025-09-03 18:16:02 -0400

    initial commit

---
+++ app/routes.py
@@ -0,0 +1,20 @@
+from flask import render_template
+from app import app
+
+# if the browser requests either of these two IRLs, Flask is going to invoke this function and return values back to the browser
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Mike'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+
+    return render_template('index.html', title='Yo', user=user, posts=posts)
