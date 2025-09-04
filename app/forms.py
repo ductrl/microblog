2a8c1b4e0c88eaa9b43db8b4d1e59bcdfb2fa005
Mike Ly <mikely@Mikes-MacBook-Pro.local>
2025-09-03 23:56:04 -0400

    added login template

--- app/forms.py
+++ app/forms.py
@@ -6,4 +6,4 @@ def LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired])
     password = PasswordField('Password', validators=[DataRequired])
     remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Submit')
+    submit = SubmitField('Sign In')
