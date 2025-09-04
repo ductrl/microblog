bc3a77a1956bc420082e921919b6e45df5a41791
Mike Ly <mikely@Mikes-MacBook-Pro.local>
2025-09-03 23:59:05 -0400

    added login page

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app import LoginForm
 
 # if the browser requests either of these two IRLs, Flask is going to invoke this function and return values back to the browser
 @app.route('/')
@@ -18,3 +19,9 @@ def index():
     ]
 
     return render_template('index.html', title='Yo', user=user, posts=posts)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+
+    return render_template('login.html', title='Sign In', form=form)
